da781ac7b563379af572e9984373c6e93d6a5b9f
Sergey-aw <sergey@berisamocat.ru>
2024-12-07 14:25:51 +0300

    chande font-size to prevent zoom on mobile devices

--- soyuz/app/join/page.tsx
+++ soyuz/app/join/page.tsx
@@ -12,8 +12,8 @@ export default function Home() {
        width={250}
        height={50}
        ></Image></Link></div>
-       <div className="text-center text-base font-forma sm:w-full sm:p-4 md:w-1/2 md:min-w-1/3 lg:w-1/3 p-4 container mx-auto">Мы проектируем коворкинг в <Link href="https://soyuzspace.ru" className="underline hover:text-blue-700" target="_blank">пространстве Союз</Link> в Туле и хотим чуть лучше узнать будущих резидентов и учесть пожелания. Будем рады вашим комментариям.</div>
-      <div className="text-3xl mx-auto text-center mt-4 font-formamedium mb-4">Поделитесь мнением</div>
+       <div className="text-center text-base font-forma sm:w-full sm:p-4 md:w-1/2 md:min-w-1/3 lg:w-1/3 p-8 container mx-auto">Мы проектируем коворкинг в <Link href="https://soyuzspace.ru" className="underline hover:text-blue-700" target="_blank">пространстве Союз</Link> в Туле и хотим чуть лучше узнать будущих резидентов и учесть пожелания. Будем рады вашим комментариям.</div>
+      <div className="text-3xl mx-auto text-center mt-8 font-formamedium mb-4">Поделитесь мнением</div>
     <div className="sm:w-full sm:p-4 md:w-1/2 md:min-w-1/3 lg:w-1/3 p-4 container mx-auto text-left">
      
     <SendForm/>
